1dbcf566c000687a37709568f067bdc51142695b
Wes Todd <wes@wesleytodd.com>
2013-02-16 09:54:09 -0600

    Moving drivers into a plugin driven archetecture

---
+++ javascript/plugins/jquery/universal-forms-jquery.js
@@ -0,0 +1,18 @@
+(function(UniversalForms, $) {
+
+	/**
+	 * Loads a form via Ajax
+	 */
+	$.getForm = function() {
+		
+	};
+
+	/**
+	 * Serialize a form
+	 */
+	$.fx.serializeForm = function() {
+		
+	};
+
+
+})(UniversalForms, jQuery);
